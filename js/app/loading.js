f2bab2ed27749a105c65bc3e4ff2b0dd9145f1f7
AIRTucha <airtucha@yandex.ru>
2015-10-15 14:05:55 +0200

    cleaned-up code

--- js/app/loading.js
+++ js/app/loading.js
@@ -19,120 +19,206 @@ var Snap = require('snap');
  */
 function Loading() {
 
-    var w = $(window).width();
-    var h = $(window).height();
+  var w = $(window).width();
+  var h = $(window).height();
 
-    $("body").css("overflow", "hidden")
-             .css("background", "black");
+  $("body").css("overflow", "hidden")
+           .css("background", "black");
     
-    var s = Snap("#main").attr({
-                width  : w,
-                height : h
-    }); 
+  var s = Snap("#main").attr({
+                              width  : w,
+                              height : h
+                            }); 
 	
-		var bg = s.rect(0, 0, w, h);
-    		bg.attr({ fill : s.gradient("r(0.5, 0.5, 1)#33305f-#000") });
+	var bg = s.rect(0, 0, w, h);
+    bg.attr({ fill : s.gradient("r(0.5, 0.5, 1)#30385f-#000") });
 	
-    var frame = s.rect(w*0.4, h*0.35, w*0.2, h*0.2);             
-				frame.attr({
-						fill : "black",
-						stroke : "white",
-						"fill-opacity" : 0
-				});
+  var frame = s.rect(w*0.4, h*0.35, w*0.2, h*0.2);             
+    frame.attr({
+                fill : "black",
+                stroke : "white",
+                "fill-opacity" : 0
+              });
 
-    var loading = s.text(w*0.5, h*0.45, "Loading...");       
-				loading.attr({
-						fill : "white",
-						"text-anchor" : "middle",
-						"font-size" : w * 0.02
-				});
+  var loading = s.text(w*0.5, h*0.45, "Loading...\n USCT");       
+    loading.attr({
+                  fill : "white",
+                  "text-anchor" : "middle",
+                  "font-size" : w * 0.02
+                 });
 
-    var pLeftFrame  = creatHalfFrame(w*0.4, h*0.4, 1);
-    var pRightFrame = creatHalfFrame(w*0.6, h*0.4, -1);
-    
-    var lineUp = s.line(w*0.5, 0, w*0.5, h*0.35);
-    		lineUp.attr({stroke : "white"});
-    
-    var lineDown = s.line(w*0.5, h,w*0.5, h*0.55);
-    		lineDown.attr({stroke : "white"});  
+  var pLeftFrame  = creatHalfFrame(w*0.4, h*0.4, 1);
+  var pRightFrame = creatHalfFrame(w*0.6, h*0.4, -1);
+
+  var lineUp = s.line(w*0.5, 0, w*0.5, h*0.35);
+    lineUp.attr({stroke : "white"});
+
+  var lineDown = s.line(w*0.5, h,w*0.5, h*0.55);
+    lineDown.attr({stroke : "white"});  
 	
 	//create corners
-		var cLeftTop = s.polyline([0, h*0.03-4, 0, h*0.07, -4, h*0.07-4, -4, h*0.03-4, w*0.03-8, h*0.03-4, w*0.03, h*0.03, 0, h*0.03]);
-           cLeftTop.attr({fill : "#fff",stroke : "#fff"});  
-           cLeftTop.attr({transform : 'translate('+w*0.4+','+h*0.32+')'});
+	var cLeftTop = s.polyline([
+                             0, h*0.03-6,
+                             0, h*0.07, -6,
+                             h*0.07-6, -6,
+                             h*0.03-6, w*0.03-12,
+                             h*0.03-6, w*0.03,
+                             h*0.03, 0, h*0.03
+                            ]);
+    cLeftTop.attr({
+                   fill : "#fff",
+                   stroke : "#fff"
+                  });  
+    cLeftTop.attr({transform : 'translate('+w*0.4+','+h*0.32+')'});
     
     
-		var cRightTop = s.polyline([0, h*0.03-4, 0, h*0.07, 4, h*0.07-4, 4, h*0.03-4, 8-w*0.03, h*0.03-4, -w*0.03, h*0.03, 0, h*0.03]);
-           cRightTop.attr({fill : "#fff", stroke : "#fff"});  
-           cRightTop.attr({transform:'translate(' + w*0.6 + ',' + h*0.32 + ')'});
+  var cRightTop = s.polyline([
+                              0, h*0.03-6,
+                              0, h*0.07, 6,
+                              h*0.07-6, 6,
+                              h*0.03-6, 12-w*0.03,
+                              h*0.03-6, -w*0.03,
+                              h*0.03, 0, h*0.03
+                             ]);
+    cRightTop.attr({
+                    fill : "#fff",
+                    stroke : "#fff"
+                   });  
+    cRightTop.attr({transform:'translate(' + w*0.6 + ',' + h*0.32 + ')'});
     
-		var cLeftBottom = s.polyline([0, 4-h*0.03, 0,-h*0.07, -4, 4-h*0.07, -4, 4-h*0.03, w*0.03-8, 4-h*0.03, w*0.03, -h*0.03, 0, -h*0.03]);
-           cLeftBottom.attr({fill : "#fff", stroke : "#fff"});  
-           cLeftBottom.attr({transform : 'translate('+w*0.4+','+h*0.58+')'});
+  var cLeftBottom = s.polyline([  
+                                0, 6-h*0.03,
+                                0,-h*0.07, -6,
+                                6-h*0.07, -6,
+                                6-h*0.03, w*0.03-12,
+                                6-h*0.03, w*0.03,
+                                -h*0.03, 0, -h*0.03
+                               ]);
+    cLeftBottom.attr({
+                      fill : "#fff",
+                      stroke : "#fff"
+                    });  
+    cLeftBottom.attr({transform : 'translate('+w*0.4+','+h*0.58+')'});
     
     
-		var cRightBottom = s.polyline([0, 4-h*0.03, 0, -h*0.07, 4, 4-h*0.07, 4, 4-h*0.03, 8-w*0.03, 4-h*0.03, -w*0.03, -h*0.03, 0, -h*0.03]);
-           cRightBottom.attr({fill : "#fff", stroke : "#fff"});  
-           cRightBottom.attr({transform : 'translate(' + w*0.6 + ',' + h*0.58 + ')'});
+  var cRightBottom = s.polyline([ 
+                                 0, 6-h*0.03,
+                                 0, -h*0.07,
+                                 6, 6-h*0.07,
+                                 6, 6-h*0.03,
+                                 12-w*0.03, 6-h*0.03,
+                                 -w*0.03, -h*0.03,
+                                 0, -h*0.03
+                                ]);
+    cRightBottom.attr({
+                        fill : "#fff",
+                        stroke : "#fff"
+                      });  
+    cRightBottom.attr({transform : 'translate(' + w*0.6 + ',' + h*0.58 + ')'});
            
-    setTimeout(function(){ 
-			
-			//animation for the end of loading
-					 frame.animate({x : w*0.05,
-													y : window.innerHeight*0.035,
-													width : w*0.9,
-													height : window.innerHeight*0.9} ,
-													1000, mina.easeinout);
-                        
-            cLeftTop.animate({transform : 'translate(' + w*0.05 + ',' + h*0.005 + ')'}, 1000, mina.easeinout);
-            cRightTop.animate({transform : 'translate(' + w*0.95 + ',' + h*0.005 + ')'}, 1000, mina.easeinout);
-            cLeftBottom.animate({transform : 'translate(' + w*0.05 + ',' + h*0.965 + ')'}, 1000, mina.easeinout);
-            cRightBottom.animate({transform : 'translate(' + w*0.95 + ',' + h*0.965 + ')'}, 1000, mina.easeinout);
-            
-            pLeftFrame.animate({transform : 'translate(' + w*0.05 + ',' + h*0.4 + ')'}, 1000, mina.easeinout);
-            pRightFrame.animate({transform : 'translate(' + w*0.95 + ',' + h*0.4 + ')'}, 1000, mina.easeinout);
-            lineUp.animate({transform : 'translate(' + 0 + ',' + -h*0.315 + ')'}, 1000, mina.easeinout);
-            lineDown.animate({transform  : 'translate(' + 0 + ',' + h*0.385 + ')'}, 1000, mina.easeinout);
-            
-            loading.remove();           
-    }, 1000);
+  setTimeout(function(){ 
+    var animationMode = mina.easeinout;
+    var duration = 300;
+    
+    //animation for the end of loading
+    frame.animate({
+                    x : w*0.05,
+                    y : window.innerHeight*0.035,
+                    width : w*0.9,
+                    height : window.innerHeight*0.9
+                    },
+                    duration,animationMode
+                   );
 
- /**
- * DESCRIPTION
- * @function create image of sophisticated part of the frame 
- * @param x, y {int} coordinates position
- * @param d {int} scaling on x axis
- */
-    function creatHalfFrame(x, y, d){     
-                        
-        var pLeftS = s.polyline([ 1*d, h*0.03, 1*d, h*0.07, -1*d, h*0.07-3, -1*d, h*0.03+3]);
-            pLeftS.attr({
-												 fill   : "#000",
-												 stroke : "#000"
-												});    
+          cLeftTop.animate({
+                            transform : 'translate(' + w*0.05 + ',' + h*0.005 + ')'
+                           },
+                            duration, animationMode);
+          cRightTop.animate({
+                             transform : 'translate(' + w*0.95 + ',' + h*0.005 + ')'
+                            },
+                             duration, animationMode);
+          cLeftBottom.animate({
+                               transform : 'translate(' + w*0.05 + ',' + h*0.965 + ')'
+                              },
+                               duration, animationMode);
+          cRightBottom.animate({
+                                transform : 'translate(' + w*0.95 + ',' + h*0.965 + ')'
+                               },
+                                duration, animationMode);
+
+          pLeftFrame.animate({
+                              transform : 'translate(' + w*0.05 + ',' + h*0.4 + ')'
+                             },
+                             duration, animationMode);
+          pRightFrame.animate({
+                               transform : 'translate(' + w*0.95 + ',' + h*0.4 + ')'
+                              },
+                               duration, animationMode);
+          lineUp.animate({
+                          transform : 'translate(' + 0 + ',' + -h*0.315 + ')'
+                         },
+                          duration, animationMode);
+          lineDown.animate({
+                            transform  : 'translate(' + 0 + ',' + h*0.385 + ')'
+                           },
+                           duration, animationMode);
+
+          loading.remove();           
+  }, 1000);
+
+   /**
+   * DESCRIPTION
+   * @function create image of sophisticated part of the frame 
+   * @param x, y {int} coordinates position
+   * @param d {int} scaling on x axis
+   */
+  function creatHalfFrame(x, y, d){     
+
+    var pLeftS = s.polyline([
+                             d, h*0.03,
+                             d, h*0.07,
+                             -1*d, h*0.07-4,
+                             -1*d, h*0.03+4
+                            ]);
+      pLeftS.attr({
+                   fill   : "#000",
+                   stroke : "#000"
+                  });    
 
 
-        var pLeftM = s.polyline([0*d, -0.02*h, 0*d, h*0.12, -4*d, h*0.12-4, -4*d, 4-0.02*h]);
-            pLeftM.attr({
-												 fill   : "white",
-												 stroke : "white"
-												}); 
+    var pLeftM = s.polyline([
+                             0, -0.02*h,
+                             0, h*0.12,
+                             -6*d, h*0.12-6,
+                             -6*d, 6-0.02*h
+                            ]);
+      pLeftM.attr({
+                   fill   : "white",
+                   stroke : "white"
+                  }); 
 
-        var pLeftB = s.polyline([0*d, -h*0.05, 0*d, h*0.15, 5*d, h*0.15-5, 5*d, -h*0.05+5, 0*d,-h*0.05]);    
-            pLeftB.attr({
-												stroke  : "white",
-												"opacity" : 0.5,
-												"fill-opacity" : 0
-						});
+    var pLeftB = s.polyline([
+                             0, -h*0.05,
+                             0, h*0.15,
+                             10*d, h*0.15-10,
+                             10*d, -h*0.05+10,
+                             0,-h*0.05
+                            ]);    
+      pLeftB.attr({
+                    stroke  : "white",
+                    "opacity" : 0.5,
+                    "fill-opacity" : 0
+                  });
 
-        var lineLeft = s.line(-w*0.4*d, h*0.05, -5*d, h*0.05);
-            lineLeft.attr({stroke : "white"});
-            
-        var halfFrame = s.group(pLeftB, pLeftM, pLeftS, lineLeft);        
-            halfFrame.attr({transform:'translate(' + x + ',' + y + ')'});
-            
-            return    halfFrame
-        }
+    var lineLeft = s.line(-w*0.4*d, h*0.05, -5*d, h*0.05);
+      lineLeft.attr({stroke : "white"});
+
+    var halfFrame = s.group(pLeftB, pLeftM, pLeftS, lineLeft);        
+      halfFrame.attr({transform:'translate(' + x + ',' + y + ')'});
+
+    return halfFrame;
+  }
 }
 
 module.exports = Loading;
