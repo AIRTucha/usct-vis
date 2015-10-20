b2ff0ead653ce6024cf8dd99c9cb2d54e474bd23
AIRTucha <airtucha@yandex.ru>
2015-10-20 13:40:30 +0200

    tomo breast visualisation

--- js/app/loading.js
+++ js/app/loading.js
@@ -5,131 +5,141 @@
 var $ = require('jquery');
 var Snap = require('snap');
 
-function Loading() {
-
+function Loading(callback) {
+  
   var w = $(window).width();
   var h = $(window).height();
-
-  $("body").css("overflow", "hidden")
-           .css("background", "black");
-    
+  
   var s = Snap("#main").attr({
     width  : w,
     height : h
-  }); 
-	
+  });
+  
   var bg = s.rect(0, 0, w, h);
+  var frame = s.rect(w*0.4, h*0.35, w*0.2, h*0.2);
   
-  bg.attr({ fill : s.gradient("r(0.5, 0.5, 1)#30385f-#000") });
-	
-	
-  var frame = s.rect(w*0.4, h*0.35, w*0.2, h*0.2);  
-  
-  frame.attr({
-    fill : "black",
-    stroke : "white",
-    "fill-opacity" : 0
-  });
- 
- 
-  var loading = s.text(w*0.5, h*0.46, "USCT Loading..."); 
-  
-  loading.attr({
-    fill : "white",
-    "text-anchor" : "middle",
-    "font-size" : w * 0.02, 
-  });
-
-
-  var pLeftFrame  = creatHalfFrame(w*0.4, h*0.4, 1);
-  
+  var pLeftFrame  = creatHalfFrame(w*0.4, h*0.4, 1);  
   var pRightFrame = creatHalfFrame(w*0.6, h*0.4, -1);
-
-
+  
   var lineUp = s.line(w*0.5, 0, w*0.5, h*0.35);
+  var lineDown = s.line(w*0.5, h,w*0.5, h*0.55);  
   
-  lineUp.attr({stroke : "white"});
-  
-
-  var lineDown = s.line(w*0.5, h,w*0.5, h*0.55);
-  
-  lineDown.attr({stroke : "white"});  
-	
-	//create corners
   var cLeftTop = s.polyline([
-    0, h*0.03-6,
-    0, h*0.07, -6,
-    h*0.07-6, -6,
-    h*0.03-6, w*0.03-12,
-    h*0.03-6, w*0.03,
-    h*0.03, 0, h*0.03
-  ]);
+     0, h*0.03-6,
+     0, h*0.07, -6,
+     h*0.07-6, -6,
+     h*0.03-6, w*0.03-12,
+     h*0.03-6, w*0.03,
+     h*0.03, 0, h*0.03
+    ]);
   
-  cLeftTop.attr({
-    fill : "#fff",
-    stroke : "#fff"
-  });  
-  
-  cLeftTop.attr({transform : 'translate('+w*0.4+','+h*0.32+')'});
-    
-    
   var cRightTop = s.polyline([
-    0, h*0.03-6,
-    0, h*0.07, 6,
-    h*0.07-6, 6,
-    h*0.03-6, 12-w*0.03,
-    h*0.03-6, -w*0.03,
-    h*0.03, 0, h*0.03
-   ]);
-  
-  cRightTop.attr({
-    fill : "#fff",
-    stroke : "#fff"
-  }); 
-  
-  cRightTop.attr({transform:'translate(' + w*0.6 + ',' + h*0.32 + ')'});
-    
+      0, h*0.03-6,
+      0, h*0.07, 6,
+      h*0.07-6, 6,
+      h*0.03-6, 12-w*0.03,
+      h*0.03-6, -w*0.03,
+      h*0.03, 0, h*0.03
+     ]);
   
   var cLeftBottom = s.polyline([  
-    0, 6-h*0.03,
-    0,-h*0.07, -6,
-    6-h*0.07, -6,
-    6-h*0.03, w*0.03-12,
-    6-h*0.03, w*0.03,
-    -h*0.03, 0, -h*0.03
-  ]);
+      0, 6-h*0.03,
+      0,-h*0.07, -6,
+      6-h*0.07, -6,
+      6-h*0.03, w*0.03-12,
+      6-h*0.03, w*0.03,
+      -h*0.03, 0, -h*0.03
+    ]);
   
-  cLeftBottom.attr({
-    fill : "#fff",
-    stroke : "#fff"
-  });  
-  
-  cLeftBottom.attr({transform : 'translate('+w*0.4+','+h*0.58+')'});
-    
-    
   var cRightBottom = s.polyline([ 
-   0, 6-h*0.03,
-   0, -h*0.07,
-   6, 6-h*0.07,
-   6, 6-h*0.03,
-   12-w*0.03, 6-h*0.03,
-   -w*0.03, -h*0.03,
-   0, -h*0.03
-  ]);
+     0, 6-h*0.03,
+     0, -h*0.07,
+     6, 6-h*0.07,
+     6, 6-h*0.03,
+     12-w*0.03, 6-h*0.03,
+     -w*0.03, -h*0.03,
+     0, -h*0.03
+    ]);
   
-  cRightBottom.attr({
-    fill : "#fff",
-    stroke : "#fff"
-  }); 
+  var loaded = false;
   
-  cRightBottom.attr({transform : 'translate(' + w*0.6 + ',' + h*0.58 + ')'});
-      
-           
-  setTimeout(function(){ 
-    var animationMode = mina.easeinout;
-    var duration = 300;
+  var loadingIcon;
+  var logo; 
+  
+  $(window).ready(function(){
+    init(callback);
+  });
     
-    //animation for the end of loading
+ /**
+ * @functions set parametrs for elements of animation
+ */   
+  function init(callback){
+    //make app almost full screen
+    $("body").css("overflow", "hidden")
+             .css("background", "black");  
+
+    bg.attr({ fill : s.gradient("r(0.5, 0.5, 1)#30385f-#000") });
+	  
+    frame.attr({
+      fill : "black",
+      stroke : "white",
+      "fill-opacity" : 0
+    });  
+    
+    // loads logo and init all other elements after that
+    Snap.load("/logo.svg", function(f){
+      logo=f.select('#usct-logo')
+      logo.scale = w/4000;
+      s.append(logo);
+
+      logo.transform('matrix('
+        + logo.scale + ',0,0,' + logo.scale + ','
+        + w*0.405 + ',' + h*0.425 + ')'
+      );    
+
+      //set vertical lines on top and bottom
+      lineUp.attr({stroke : "white"}); 
+      lineDown.attr({stroke : "white"}); 		
+
+      //set corners
+      cLeftTop.attr({
+       fill : "#fff",
+       stroke : "#fff"
+      });  
+      cLeftTop.attr({transform : 'translate('+w*0.4+','+h*0.32+')'});
+
+      cRightTop.attr({
+        fill : "#fff",
+        stroke : "#fff"
+      }); 
+      cRightTop.attr({transform:'translate(' + w*0.6 + ',' + h*0.32 + ')'});
+
+      cLeftBottom.attr({
+        fill : "#fff",
+        stroke : "#fff"
+      });  
+      cLeftBottom.attr({transform : 'translate('+w*0.4+','+h*0.58+')'});   
+
+      cRightBottom.attr({
+        fill : "#fff",
+        stroke : "#fff"
+      }); 
+      cRightBottom.attr({transform : 'translate(' + w*0.6 + ',' + h*0.58 + ')'});    
+       
+      // animation starts
+      setTimeout(function(){ 
+        startLoadingAnimation(250, mina.easeinout, callback)
+      }, 500);            
+    });
+  }
+ /**
+ * @functions start loading animation
+ * @param int duration in ms
+ * @param animation mode for Snap
+ * @param callback executed when the loading is done
+ */ 
+  function startLoadingAnimation(duration, animationMode, callback){ 
+    //main frame animation
     frame.animate({
       x : w*0.05,
       y : window.innerHeight*0.035,
@@ -139,21 +149,22 @@ function Loading() {
       duration,animationMode
      );
 
+    //corners
     cLeftTop.animate({
       transform : 'translate(' + w*0.05 + ',' + h*0.005 + ')'
      },
       duration, animationMode);
-    
+
     cRightTop.animate({
      transform : 'translate(' + w*0.95 + ',' + h*0.005 + ')'
     },
      duration, animationMode);
-    
+
     cLeftBottom.animate({
      transform : 'translate(' + w*0.05 + ',' + h*0.965 + ')'
     },
      duration, animationMode);
-    
+
     cRightBottom.animate({
       transform : 'translate(' + w*0.95 + ',' + h*0.965 + ')'
     },
@@ -167,40 +178,54 @@ function Loading() {
      transform : 'translate(' + w*0.95 + ',' + h*0.4 + ')'
     },                        
      duration, animationMode);
-    
+
+
+    // vertical lines on top and on bottom
     lineUp.animate({
       transform : 'translate(' + 0 + ',' + -h*0.315 + ')'
     },
       duration, animationMode);
-    
+
     lineDown.animate({
       transform  : 'translate(' + 0 + ',' + h*0.385 + ')'
     },
      duration, animationMode);
 
-    loading.remove();           
-  }, 1000);
 
+    //logo moved to corner
+    logo.animate({
+      transform : 'matrix('
+      + logo.scale + ',0,0,' + logo.scale + ','
+      + w*0.75 + ',' + h*0.87 + ')'
+    },
+      duration, animationMode,
+      function(){
+        //start loading icon animation
+        loadingIcon = creatLoadingIcon(w*0.5, h*0.47); 
+        startIconAnimation(mina.easeinout, 250);
+      
+        callback()
+      }
+    );
+
+   
+         
+  } 
+  
    /**
-   * @function create image of sophisticated part of the frame 
+   * @function create image of part of the frame 
    * @param x, y {int} coordinates position
    * @param d {int} scaling on x axis
    */
   function creatHalfFrame(x, y, d){     
 
     var pLeftS = s.polyline([
-      d, h*0.03,
-      d, h*0.07,
-      -1*d, h*0.07-4,
-      -1*d, h*0.03+4
-    ]);
-    
-    pLeftS.attr({
-      fill   : "#000",
-      stroke : "#000"
-    });    
-
-    
+     d, h*0.03,
+     d, h*0.07,
+     -1*d, h*0.07-4,
+     -1*d, h*0.03+4
+    ]);   
+        
     var pLeftM = s.polyline([
       0, -0.02*h,
       0, h*0.12,
@@ -208,37 +233,198 @@ function Loading() {
       -6*d, 6-0.02*h
     ]);
 
-    pLeftM.attr({
-      fill   : "white",
-      stroke : "white"
-    }); 
-
-
     var pLeftB = s.polyline([
-      0, -h*0.05,
-      0, h*0.15,
-      10*d, h*0.15-10,
-      10*d, -h*0.05+10,
-      0,-h*0.05
-    ]);    
+     0, -h*0.05,
+     0, h*0.15,
+     10*d, h*0.15-10,
+     10*d, -h*0.05+10,
+     0,-h*0.05
+    ]); 
+    
+    var lineLeft = s.line(-w*0.4*d, h*0.05, -5*d, h*0.05);
+    var halfFrame = s.group(pLeftB, pLeftM, pLeftS, lineLeft);
     
     pLeftB.attr({
       stroke  : "white",
       "opacity" : 0.5,
       "fill-opacity" : 0
     });
-
-
-    var lineLeft = s.line(-w*0.4*d, h*0.05, -5*d, h*0.05);
     
     lineLeft.attr({stroke : "white"});
-
-    var halfFrame = s.group(pLeftB, pLeftM, pLeftS, lineLeft);        
+    setColor('#000020')(pLeftS);
+    setColor('#ffffff')(pLeftM);
     
     halfFrame.attr({transform:'translate(' + x + ',' + y + ')'});
-
+        
     return halfFrame;
   }
+  
+  /**
+  * @function create loading icon
+  * @param x, y {int} coordinates position    
+  */
+  function creatLoadingIcon(x, y){
+    x-=h*0.02; // centralizetion 
+    
+    var tl = [x, y];              // top left corner
+    var tr = [x+h*0.04, y];       // top right corner
+    var c = [x+h*0.02, y+h*0.02]; // center
+    var bl = [x, y+h*0.04];       // bottom left
+    var br = [x+h*0.04, y+h*0.04];// bottom right 
+        
+    var top = s.polyline([tl, tr, c]);    
+    var right = s.polyline([tr, br, c]);    
+    var bottom = s.polyline([bl, br, c]);    
+    var left = s.polyline([bl, tl, c]);  
+      
+    setColor('#ffffff')(top);
+    setColor('#ffffff')(bottom);
+    setColor('#ffffff')(left);
+    setColor('#ffffff')(right);
+       
+    var icon = s.group(top, bottom, left, right);  
+    
+    icon.top = top;
+    icon.bottom = bottom;
+    icon.left = left;
+    icon.right = right;
+        
+    icon.tl = tl;
+    icon.tr = tr;
+    icon.c  = c;
+    icon.bl = bl;
+    icon.br = br;
+        
+    return icon;    
+  }
+  
+  /**
+  * @function start animation for loading icon
+  * @param animation mode for Snap
+  * @param int duration 
+  */
+  function startIconAnimation(animationMode, duration){
+   //step one
+    loadingIcon.right.animate({
+      transform : 'rotate(' + 90 +', ' + loadingIcon.c[0] +', ' + loadingIcon.c[1] + ')'
+    },
+       duration,animationMode
+    );
+        
+    loadingIcon.left.animate({
+      transform : 'rotate(' + 90 +', ' + loadingIcon.c[0] +', ' + loadingIcon.c[1] + ')'
+    },                           
+      duration,animationMode
+    );   
+    
+    if(!loaded)
+      setTimeout(function(){
+        loadingStepTwo(animationMode, duration)
+      }, duration*3);    
+  }
+  
+  /**
+  * @functions different steps of loading icon's animation
+  */
+  function loadingStepTwo(animationMode, duration){  
+    loadingIcon.right.animate({
+      transform : 'rotate(' + 180 +', ' + loadingIcon.c[0] +', ' + loadingIcon.c[1] + ')'
+      },
+       duration,animationMode,
+       function(){
+        loadingIcon.right.attr({
+          transform : 'rotate(' + 0 +', ' + loadingIcon.c[0] +', ' + loadingIcon.c[1] + ')'
+        });
+       }
+    );
+        
+    loadingIcon.left.animate({
+      transform : 'rotate(' + 180 +', ' + loadingIcon.c[0] +', ' + loadingIcon.c[1] + ')'
+      },                           
+      duration,animationMode,
+      function(){
+        loadingIcon.left.attr({
+          transform : 'rotate(' + 0 +', ' + loadingIcon.c[0] +', ' + loadingIcon.c[1] + ')'
+        });
+      }                             
+    );        
+    
+    if(!loaded)
+      setTimeout(function(){
+        loadingStepThree(animationMode, duration)
+      }, duration*3);
+  }
+  function loadingStepThree(animationMode, duration){  
+    loadingIcon.top.animate({
+      transform : 'rotate(' + 90 +', ' + loadingIcon.c[0] +', ' + loadingIcon.c[1] + ')'
+    },
+       duration,animationMode
+    );
+        
+    loadingIcon.bottom.animate({
+      transform : 'rotate(' + 90 +', ' + loadingIcon.c[0] +', ' + loadingIcon.c[1] + ')'
+    },                           
+      duration,animationMode
+    );
+    
+    if(!loaded)
+      setTimeout(function(){
+        loadingStepFour(animationMode, duration)
+      }, duration*3);
+  }
+  function loadingStepFour(animationMode, duration){  
+    loadingIcon.top.animate({
+      transform : 'rotate(' + 180 +', ' + loadingIcon.c[0] +', ' + loadingIcon.c[1] + ')'
+    },
+       duration,animationMode,
+      function(){
+        loadingIcon.top.attr({
+          transform : 'rotate(' + 0 +', ' + loadingIcon.c[0] +', ' + loadingIcon.c[1] + ')'
+        });
+      }             
+    );
+        
+    loadingIcon.bottom.animate({
+      transform : 'rotate(' + 180 +', ' + loadingIcon.c[0] +', ' + loadingIcon.c[1] + ')'
+    },                           
+      duration,animationMode,
+      function(){
+        loadingIcon.bottom.attr({
+          transform : 'rotate(' + 0 +', ' + loadingIcon.c[0] +', ' + loadingIcon.c[1] + ')'
+        });
+      }                   
+    );
+    
+    if(!loaded)
+      setTimeout(function(){
+        startIconAnimation(animationMode, duration)
+      }, duration*3);
+  }
+    
+  /**
+  * @function function returns function which keeps color in string as closure
+  * @param color - color in string format
+  * @curr Snap.svg object - the element you want to set to the color 
+  */
+  function setColor(color){
+    return function sc(o){
+      o.attr({
+        fill   : color
+      });
+    }
+  }
+  
+  /**
+  * @function turn off the loading icon
+  */
+  this.stopIcon = function (){  
+    loadingIcon.top.remove();
+    loadingIcon.right.remove();
+    loadingIcon.bottom.remove();
+    loadingIcon.left.remove();
+
+    loaded = true;
+  } 
 }
 
 module.exports = Loading;
--- js/app/main.js
+++ js/app/main.js
@@ -11,4 +11,50 @@ console.log("Main file loaded");
 // and if user switch data
 // we can do loading.loadData(<PATH>);
 // and a loading bar with appear, but dont redo the splash animation
-var loading = new Loading();
+var loading = new Loading(function(){
+ 
+  $('#container').css({"x":$(window).width()*0.5-256,"y":$(window).height()*0.5-256})    
+             
+			config = {
+				"dom_container_id": "container",
+				"slicemaps_paths": [
+		                'data/new_data_USCT1.png',
+                    'data/new_data_USCT2.png',
+                    'data/new_data_USCT3.png',
+                    'data/new_data_USCT4.png',
+                    'data/new_data_USCT5.png',
+                    'data/new_data_USCT6.png',
+                    'data/new_data_USCT7.png',
+                    'data/new_data_USCT8.png',
+                    'data/new_data_USCT9.png'
+				],
+        "callback" : function(){
+        setTimeout(function(){loading.stopIcon()}, 500); 
+        },
+				"slices_range": [0, 144],
+				"gray_min": 0.05,
+				"gray_max": 0.45,
+				"row_col": [4, 4],
+				"steps": 100,
+				"render_size": [512, 512],
+				"renderer_canvas_size": [512, 512],
+				"absorption_mode": 1,
+				"opacity_factor": 4,
+				"color_factor": 2,
+				"x_min": 0.0,
+				"x_max": 1.0,
+				"auto_steps": true,
+				"tf_colors": [
+	                {"pos": 0.25,"color": "#7CFF79"},
+	                {"pos": 0.5, "color": "#FF9400"},
+	                {"pos": 1.0, "color": "#7F0000"},
+	            ],
+                "refl":0.4,
+                "sat" :0.8,
+                "sos" :1
+                
+			};
+  
+   rcl2 = new VRC.VolumeRaycaster(config);
+  
+});
